55976756fa85446dd28bea3436e271f724d12bd0
OpenSauce04 <opensauce04@gmail.com>
2024-11-29 17:32:07 +0000

    Restructed emacs config for Cask + Added 'hl-todo' emacs package

---
+++ .emacs.d/init.el
@@ -0,0 +1,43 @@
+
+
+
+;;==== VISUAL TWEAKS =================================================================================
+;;# Basic visual settings
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(set-scroll-bar-mode 'right)
+(global-display-line-numbers-mode 1)
+(setq truncate-lines 1)
+
+;; Disable splash screen and startup message
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+;; Load theme
+(load-theme 'tango-dark)
+
+
+
+;;==== PACKAGES =====================================================================================
+;; Shut up package-initialize warning
+(setq warning-suppress-log-types '((package reinitialization)))
+
+;; Start Cask and load installed packages
+(require 'cask "/usr/share/emacs/site-lisp/cask/cask.el")
+(cask-initialize)
+
+
+
+;;==== BEHAVIOUR TWEAKS ===============================================================================
+;; Disable customize
+(setq custom-file "/dev/null")
+
+;; Disable backups
+(setq make-backup-files nil)
+
+;; Enable ido mode
+(require 'ido)
+(ido-mode t)
+
+;; Enable TODO highlighting
+(global-hl-todo-mode 1)
