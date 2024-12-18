3266c8762e5d7bdbe0a1c73fa4c4c679bb18fe63
OpenSauce04 <opensauce04@gmail.com>
2024-12-18 11:39:30 +0000

    emacs: Better C-Backspace behaviour

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -118,6 +118,18 @@
 ;; Disable electric indent
 (electric-indent-mode 0)
 
+;; Better C-Backspace behaviour
+(defun my-backward-kill-spaces-or-char-or-word ()
+  (interactive)
+  (cond
+   ((looking-back (rx (char word)) 1)
+    (backward-kill-word 1))
+   ((looking-back (rx (char blank)) 1)
+    (delete-horizontal-space t))
+   (t
+    (backward-delete-char 1))))
+(global-set-key (kbd "<C-backspace>") 'my-backward-kill-spaces-or-char-or-word)
+
 
 
 
