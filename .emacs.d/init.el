8d79ab2f5cc5f2e1805baa810e5df79bc09f4834
OpenSauce04 <opensauce04@gmail.com>
2025-04-22 15:18:11 +0100

    emacs: Only perform Asahi graphics workaround when using GUI mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -110,7 +110,7 @@
 (setq ring-bell-function 'ignore)
 
 ;; Force a redraw of entire Emacs window every second on ARM Linux to circumvent Asahi graphical issues
-(if (string-match "aarch64-unknown-linux-gnu" system-configuration)
+(if (and (string-match "aarch64-unknown-linux-gnu" system-configuration) (window-system))
     (run-with-timer 0 1 'redraw-display))
 
 ;; Configure markdown-preview-mode
