aab0554cdd6c9556e85b54ce27af2abf20990a33
OpenSauce04 <opensauce04@gmail.com>
2025-08-11 15:57:58 +0100

    Added a script which configures and builds Emacs

---
+++ scripts/configure-and-build-emacs.sh
@@ -0,0 +1,4 @@
+git clean -dfX
+./autogen.sh && \
+  ./configure --without-all --without-x --with-gnutls --with-modules --with-threads --with-tree-sitter --with-xml2 --enable-link-time-optimization && \
+  make -j$(getconf _NPROCESSORS_ONLN)
