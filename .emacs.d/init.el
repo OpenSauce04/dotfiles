5adac220f05b09ca7e9225624b007e6988f42cfd
OpenSauce04 <opensauce04@gmail.com>
2025-12-23 18:07:48 +0000

    emacs: Removed stray commented code from init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,10 +7,6 @@
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (package-initialize)
 
-;;(unless (package-installed-p 'use-package)
-;;  (package-refresh-contents)
-;;  (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
