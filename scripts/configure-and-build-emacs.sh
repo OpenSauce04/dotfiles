c665e28726cdc9448d7d9a7d657d31b54fa537f9
OpenSauce04 <opensauce04@gmail.com>
2026-01-25 15:51:52 +0000

    emacs: Add patch in build script which forces dark mode on macOS

--- scripts/configure-and-build-emacs.sh
+++ scripts/configure-and-build-emacs.sh
@@ -1,4 +1,9 @@
-git clean -dfX
-./autogen.sh && \
-  ./configure --without-all --with-gnutls --with-modules --with-threads --with-toolkit-scroll-bars --with-tree-sitter --enable-link-time-optimization && \
-  make -j$(getconf _NPROCESSORS_ONLN)
+#!/bin/bash -ex
+
+git reset --hard
+sudo git clean -dfX
+git apply ~/dotfiles/patches/emacs/*.patch
+
+./autogen.sh
+./configure --without-all --with-gnutls --with-modules --with-threads --with-toolkit-scroll-bars --with-tree-sitter --enable-link-time-optimization
+make -j$(getconf _NPROCESSORS_ONLN)
