547edaea7f499b2963eb1016ad6d5ec327cb2188
OpenSauce04 <opensauce04@gmail.com>
2026-03-25 19:01:55 +0000

    Added make install step to Emacs build script
    This doesn't actually install Emacs in this case, it just finishes setting up the .app bundle

--- scripts/configure-and-build-emacs.sh
+++ scripts/configure-and-build-emacs.sh
@@ -5,3 +5,4 @@ sudo git clean -dfX
 ./autogen.sh
 ./configure --without-all --with-gnutls --with-modules --with-threads --with-toolkit-scroll-bars --with-tree-sitter --enable-link-time-optimization
 make -j$(getconf _NPROCESSORS_ONLN)
+make install
