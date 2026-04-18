4f29db56522bf6944ae90bf0008b1be7cb30c9d9
OpenSauce04 <opensauce04@gmail.com>
2026-04-18 21:32:13 +0100

    Make Emacs build script compatible with OpenBSD

--- scripts/configure-and-build-emacs.sh
+++ scripts/configure-and-build-emacs.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -ex
 
+if [ "$(uname -s)" = "OpenBSD" ]; then
+  export AUTOCONF_VERSION=2.72
+  export MAKEINFO=/usr/local/bin/gmakeinfo
+  export EMACS_MAKE_JOBS=$(go-nproc)
+else
+  export EMACS_MAKE_JOBS=$(getconf _NPROCESSORS_ONLN)
+fi
+
 sudo git clean -dfX
 
 ./autogen.sh
 ./configure --without-all --with-gnutls --with-modules --with-threads --with-toolkit-scroll-bars --with-tree-sitter --enable-link-time-optimization
-make -j$(getconf _NPROCESSORS_ONLN)
-make install
+gmake -j$EMACS_MAKE_JOBS
+gmake install
